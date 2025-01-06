035e43159ce253a4acdc26191f9c3f9851001f34
Hina Zafar <hina.nida@gmail.com>
2025-01-06 20:54:58 +0500

    The code commit contains getWindowHandles() function implementation. This will return a set of strings(all elements of the set are mandatory to be different and unique.)

--- src/test/java/XPATH/xpathDemo.java
+++ src/test/java/XPATH/xpathDemo.java
@@ -5,13 +5,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class xpathDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        System.out.println(driver.getTitle());
-        System.out.println(driver.getCurrentUrl());
-        System.out.println(driver.getPageSource());
+        Thread.sleep(6000);
+        //System.out.println(driver.getTitle());
+        //System.out.println(driver.getCurrentUrl());
+        //System.out.println(driver.getPageSource());
+        driver.findElement(By.linkText("OrangeHRM, Inc")).click();
+        System.out.println(driver.getWindowHandles());
 
     }
 }
